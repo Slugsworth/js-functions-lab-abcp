b4b4706777257f06db1dbfce80e02762c9e64fa1
Rick Battipede <rbattipede@gmail.com>
2018-01-16 19:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // write your code below!
 
-
-function addNumbers(num1, num2, num3, num4) {
-  return num1 + num2 + num3 + num4
+function shout(string) {
+  return string.toUpperCase()
 }
 
-addNumbers();
