6476ce83e2ab957eb973c95bda4bdcd03be69d6a
Rick Battipede <rbattipede@gmail.com>
2018-01-16 19:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 // write your code below!
-function happyHolidays() {
-  console.log("Happy holidays!")
-}
-
-function happyHolidaysTo(){
-  var name = input
-  console.log("Happy holidays" + ${name}!)
-}
-
-function happyHolidayTo(){
-  var name = input
-  var holiday = input
-  var message = `Happy ${holiday} +${name}!`
-  
-  
-}
+console.log("Happy Holidays!")
