ed7536371babc52d48b6cd1a97bdd917f0f468da
Rick Battipede <rbattipede@gmail.com>
2018-01-16 19:57:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function happyHolidaysTo(name){
 }
 
 happyHolidaysTo("Rick");
+
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+happyHolidayTo("Christmas", "Rick");
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
+
+holidayCountdown("4", "Christmas");
