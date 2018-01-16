5b0b0e2f927032c7385c16db625d9fa5aaf20120
Rick Battipede <rbattipede@gmail.com>
2018-01-16 19:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 function happyHolidays() {
   console.log("Happy holidays!")
 }
+
 function happyHolidaysTo(){
   var name = input
   console.log("Happy holidays" + ${name}!)
+}
+
+function happyHolidayTo(){
+  var name = input
+  var holiday = input
+  console.log(`"Happy" + ${holiday} +${name}!`")
 }
